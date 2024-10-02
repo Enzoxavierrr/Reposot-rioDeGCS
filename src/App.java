d4db016b14bfd8eb764f38495450534cbd7d3826
Felipe Santos <lipe.taquari@gmail.com>
2024-10-01 21:57:13 -0300

    switch completo

--- src/App.java
+++ src/App.java
@@ -27,15 +27,29 @@ public class App {
                     break;
 
                 case 5:
-                    System.out.println(">>> Abrir Proposta de Troca <<<");
+                    System.out.println(">>> Buscar Itens <<<");
                     break;
 
                 case 6:
+                    System.out.println(">>> Abrir Proposta de Troca <<<");
+                    break;
+
+                case 7:
                     System.out.println(">>> Listar as propostas de troca do jogador logado <<<");
                     break;
 
+                case 8:
+                    System.out.println(">>> Estatísticas Gerais <<<");
+                    break;
+
+                case 9:
+                    System.out.println(">>> Funcionalidade 1 <<<");
+                    break;
+
+                case 10:
+                    System.out.println(">>> Funcionalidade 2 <<<");
+                    break;
             }
         }
-
     }
 }
