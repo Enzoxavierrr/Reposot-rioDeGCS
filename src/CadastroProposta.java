d4aee2b06bcfc13e7d915a1cef0f132617db295d
Seu Nome <seuemail@exemplo.com>
2024-10-06 15:42:22 -0300

    add CadastroProposta

--- src/CadastroProposta.java
+++ src/CadastroProposta.java
@@ -1,51 +1,93 @@
-import java.util.ArrayList;
+import java.util.Calendar;
 
 public class CadastroProposta {
-    private ArrayList<Proposta> propostas;
+    private int id;
+    private Jogador ofereceProp;
+    private Jogador recebeProp;
+    private Item oferece;
+    private Item recebe;
+    private String diaHora;
+    private boolean aceita; // Se a proposta foi aceita
+    private boolean declinada; // Se a proposta foi recusada
+    private boolean respondida; // Se a proposta foi respondida (aceita ou declinada)
 
-    public CadastroProposta() {
-        propostas = new ArrayList<>();
+    public void Proposta(Jogador ofereceProp, Jogador recebeProp, Item oferece, Item recebe) {
+        Calendar calendar = Calendar.getInstance();
+        int mes = calendar.get(Calendar.MONTH) + 1;
+
+        this.id = Item.randCode();
+        this.diaHora = calendar.get(Calendar.DAY_OF_MONTH) + "/" + mes + "/" + calendar.get(Calendar.YEAR)
+                + " - " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+        this.ofereceProp = ofereceProp;
+        this.recebeProp = recebeProp;
+        this.oferece = oferece;
+        this.recebe = recebe;
+        this.aceita = false; // Inicialmente a proposta não foi aceita
+        this.declinada = false; // Inicialmente a proposta não foi recusada
+        this.respondida = false; // Inicialmente a proposta não foi respondida
     }
 
-    public void addProposta(Proposta proposta) {
-        propostas.add(proposta);
+    // Getters
+    public int getId() {
+        return id;
     }
 
-    public void listarPropostas(Jogador jogador) {
-        for (Proposta proposta : propostas) {
-            if (proposta.getRecebeProp().equals(jogador)) {
-                System.out.println(proposta.toString());
-            }
-        }
+    public Jogador getOfereceProp() {
+        return ofereceProp;
     }
 
-    public int propostasAceitas() {
-        int cont = 0;
-        for (Proposta proposta : propostas) {
-            if (proposta.isAceita()) {
-                cont++;
-            }
-        }
-        return cont;
+    public Jogador getRecebeProp() {
+        return recebeProp;
     }
 
-    public int propostasDeclinadas() {
-        int cont = 0;
-        for (Proposta proposta : propostas) {
-            if (proposta.isDeclinada()) {
-                cont++;
-            }
-        }
-        return cont;
+    public Item getOferece() {
+        return oferece;
     }
 
-    public int propostasAguardando() {
-        int cont = 0;
-        for (Proposta proposta : propostas) {
-            if (!proposta.isRespondida()) {
-                cont++;
-            }
-        }
-        return cont;
+    public Item getRecebe() {
+        return recebe;
+    }
+
+    public String getDiaHora() {
+        return diaHora;
+    }
+
+    // Métodos que indicam o status da proposta
+    public boolean isAceita() {
+        return aceita;
+    }
+
+    public boolean isDeclinada() {
+        return declinada;
+    }
+
+    public boolean isRespondida() {
+        return respondida;
+    }
+
+    // Métodos para aceitar ou declinar a proposta
+    public void aceitar() {
+        this.aceita = true;
+        this.respondida = true;
+    }
+
+    public void declinar() {
+        this.declinada = true;
+        this.respondida = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Proposta{" +
+                "id=" + id +
+                ", ofereceProp=" + ofereceProp.getNome() +
+                ", recebeProp=" + recebeProp.getNome() +
+                ", oferece=" + oferece.getNome() +
+                ", recebe=" + recebe.getNome() +
+                ", diaHora='" + diaHora + '\'' +
+                ", aceita=" + aceita +
+                ", declinada=" + declinada +
+                ", respondida=" + respondida +
+                '}';
     }
 }
